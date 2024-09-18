924f8c6985ba62d4bd8e7f1dbb1a482ee414b18f
wser <w.web.dev@gmail.com>
2024-09-18 14:39:22 +0200

    style time and add time periods

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './purify.min.js'
 
 const c = e => console.log(e)
 const clean = e => DOMPurify.sanitize(e)
+const tauSeprtr = n => `${n}`.replace(/(?<!\.\d+)\B(?=(\d{3})+\b)/g, ".").replace(/(?<=\.(\d{3})+)\B/g, ".")
 
 const html = fetch("./vv.md")
 html
@@ -19,10 +20,26 @@ tech
 const myTime = document.getElementById("mytime")
 const initial = '1981-07-03T11:54:55.373Z'
 
-function interval(){
-  let startDate = new Date(initial)
-  let endDate = new Date()
-  return parseInt((endDate - startDate) / 1000); 
-}
-let seconds = setInterval(() => {requestAnimationFrame(()=> myTime.innerHTML = interval())},1000)
+setInterval(() => {
+  requestAnimationFrame(()=> {
+    let startDate = new Date(initial)
+    let endDate = new Date()
+    let difference = (endDate - startDate) 
+
+    let sec = parseInt(difference/1000)
+    let min = parseInt(difference/(1000*60))
+    let hrs = parseInt(difference/(1000*60*60))
+    let dys = parseInt(difference/(1000*60*60*24))
+    let mts = parseInt(difference/(1000*60*60*24*30))
+    let yrs = parseInt(difference/(1000*60*60*24*30*12))
+    myTime.innerHTML = `
+      <p>years: ${tauSeprtr(yrs)}</p>
+      <p>months: ${tauSeprtr(mts)}</p>
+      <p>days: ${tauSeprtr(dys)}</p>
+      <p>hours: ${tauSeprtr(hrs)}</p>
+      <p>minutes: ${tauSeprtr(min)}</p>
+      <p>seconds: ${tauSeprtr(sec)}</p>
+    `})
+}, 1000)
+
 
