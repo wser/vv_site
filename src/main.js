e51417d5a92b33a5f362fea4b08f1826fa0dc0f5
wser <w.web.dev@gmail.com>
2024-09-20 10:44:13 +0200

    bumpedup vite

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 import snarky from './snarky.js'
+import './purify.js'
 
 import { time } from "./time.js"
 
 const c = e => console.log(e)
-const clean = e => e//DOMPurify.sanitize(e)
 
 // add main content
 const html = fetch("./vv.md")
 html
-    .then(res=>res.text())
-    .then(t=>document.querySelector("#content").innerHTML = clean(snarky(t)))
+    .then(res => res.text())
+    .then(txt => snarky(txt))
+    .then(val => document.querySelector("#content").innerHTML = val)
     .catch(e=>c(e))
 
 // add used tech stack
-const tech = fetch("./tech.md")
-tech
-    .then(res=>res.text())
-    .then(t=>document.querySelector("#tech").innerHTML = clean(snarky(t)))
-    .catch(e=>c(e))
+// const tech = fetch("./tech.md")
+// tech
+//     .then(res=>res.text())
+//     .then(t=>document.querySelector("#tech").innerHTML = clean(snarky(t)))
+//     .catch(e=>c(e))
 
 
 time()
