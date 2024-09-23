4e8777b816c31abf63246a258976d086013ac798
wser <w.web.dev@gmail.com>
2024-09-23 19:38:34 +0200

    add md content

--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ import { light_switch } from './light_switch.js';
 const c = (e) => console.log(e);
 
 // add main content
-// const html = fetch("./vv.md")
-// html
-//     .then(res => res.text())
-//     .then(txt => snarky(txt))
-//     .then(val => document.querySelector("#content").innerHTML = val)
-//     .catch(e=>c(e))
+ const html = fetch("./vv.md")
+ html
+     .then(res => res.text())
+     .then(txt => snarky(txt))
+     .then(val => document.querySelector("#content").innerHTML = val)
+     .catch(e=>c(e))
 
 // // add used tech stack
-// const tech = fetch("./stack.md")
-// tech
-//     .then(res=>res.text())
-//     .then(txt => snarky(txt))
-//     .then(val => document.querySelector("#tech").innerHTML = val)
-//     .catch(e=>c(e))
+ const tech = fetch("./stack.md")
+ tech
+     .then(res=>res.text())
+     .then(txt => snarky(txt))
+     .then(val => document.querySelector("#tech").innerHTML = val)
+     .catch(e=>c(e))
 
 window.addEventListener('load', async (e) => {
   // display time passed
