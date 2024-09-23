464fdaa4ea80c974ca2b575533f3ae1d945a8800
wser <w.web.dev@gmail.com>
2024-09-23 10:40:59 +0200

    add light_switch

---
+++ src/light_switch.js
@@ -0,0 +1,11 @@
+export const light_switch = () => {    
+// set light_switch state
+    const colorScheme = document.getElementById("colorScheme")
+    colorScheme.addEventListener('change', e => localStorage.setItem('color-scheme', e.target.value))
+
+//get local storage scheme state
+    const scheme = localStorage.getItem('color-scheme') || 'auto'
+    // if (scheme) document.documentElement.style.setProperty('--darkmode', scheme)
+    const selected = [...colorScheme.elements].filter(e=> e.value === scheme)
+    if(selected) selected[0].checked = true;
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
 import snarky from './snarky.js'
 import { time } from "./time.js"
+import { light_switch } from "./light_switch.js"
+const c = e => console.log(e)
 
-// const c = e => console.log(e)
+const lightSwitch = `
+  <fieldset class="toggle-group" id="colorScheme">
+    <label>
+      <input type="radio" name="color-scheme" id="color-scheme-light" value="0" data-sr>
+      Light
+    </label>
+    <label>
+      <input type="radio" name="color-scheme" value="auto" checked data-sr>
+      Auto
+    </label>
+    <label>
+      <input type="radio" name="color-scheme" id="color-scheme-dark" value="1" data-sr>
+      Dark
+    </label>
+  </fieldset>
+`
 
-// // add main content
-// const html = fetch("./vv.md")
-// html
-//     .then(res => res.text())
-//     .then(txt => snarky(txt))
-//     .then(val => document.querySelector("#content").innerHTML = val)
-//     .catch(e=>c(e))
+// add main content
+const html = fetch("./vv.md")
+html
+    .then(res => res.text())
+    .then(txt => snarky(txt))
+    .then(val => document.querySelector("#content").innerHTML = lightSwitch+val)
+    .catch(e=>c(e))
 
 // // add used tech stack
 // const tech = fetch("./stack.md")
@@ -19,7 +36,11 @@ import { time } from "./time.js"
 //     .then(val => document.querySelector("#tech").innerHTML = val)
 //     .catch(e=>c(e))
 
-// display time passed
-time()
 
 
+
+window.addEventListener('load', async e => {
+    // display time passed
+    await time()
+    await light_switch()
+})
